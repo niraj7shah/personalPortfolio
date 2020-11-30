500d58ac40c37a83c7a829a49e89447e22a6dc1f
Niraj Shah <niraj7.shah@gmail.com>
2020-11-30 23:09:38 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+// Wrap every letter in a span
+var textWrapper = document.querySelector('.name');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.name .letter',
+    scale: [4,1],
+    opacity: [0,1],
+    translateZ: 0,
+    easing: "easeOutExpo",
+    duration: 950,
+    delay: (el, i) => 70*i
+  });
